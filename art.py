c5fbffabc4eeb6d99334eeac0515acf03e1ce1d1
d4az <dasithvidanage000@gmail.com>
2020-12-11 23:05:54 +0530

    Update art.py

--- art.py
+++ art.py
@@ -5,5 +5,5 @@ print("         |  This is a simple DOS attack script |")
 print("         |   Github: https://github.com/d4az   |")
 print("         |      Author: Dasith Vidanage        |")
 print("         |            Version: 0.1             |")
-print("         +---------------------------d4az------+ ")
+print("         +-------------------------d4az--------+ ")
 
